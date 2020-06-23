cedabc79ce8890b7b624abede5a20eec84c708b2
Max Weinberg <max.renaud.weinberg@gmail.com>
2020-06-23 01:08:09 -0400

    Turn off strong migrations

--- Gemfile
+++ Gemfile
@@ -22,7 +22,6 @@ gem 'pry-rails'
 gem 'pry-nav'
 
 gem 'bootsnap', '>= 1.1.0', require: false
-gem 'strong_migrations'
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 # Mark existing migrations as safe
-StrongMigrations.start_after = 20_200_614_214_140
+#StrongMigrations.start_after = 20_200_614_214_140
 
 # Set timeouts for migrations
 # If you use PgBouncer in transaction mode, delete these lines and set timeouts on the database user
-StrongMigrations.lock_timeout = 10.seconds
-StrongMigrations.statement_timeout = 1.hour
+# StrongMigrations.lock_timeout = 10.seconds
+# StrongMigrations.statement_timeout = 1.hour
 
 # Analyze tables after indexes are added
 # Outdated statistics can sometimes hurt performance
-StrongMigrations.auto_analyze = true
+# StrongMigrations.auto_analyze = true
 
 # Add custom checks
 # StrongMigrations.add_check do |method, args|
