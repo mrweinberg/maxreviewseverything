f0fc5f33b1c4e40744a9379e0d9cc7f046573c28
Max Weinberg <max.renaud.weinberg@gmail.com>
2020-10-09 15:22:52 -0400

    Color login message if admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  ADMIN_EMAILS = ['maxxthepenguin@gmail.com'].freeze
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
@@ -16,5 +16,9 @@ class User < ApplicationRecord
                           password: Devise.friendly_token[0, 20])
 
     user
+  end
+
+  def admin?
+    ADMIN_EMAILS.include?(email)
   end
 end
